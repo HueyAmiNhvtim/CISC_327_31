b68ea5e146148103dc75453d4105f205dda8dc9f
Huy Nguyen <dshk127mm@gmail.com>
2023-10-18 04:15:44 -0400

    Added the RestaurantOwner and Restaurant models

--- Megabyte/res_owner/models.py
+++ Megabyte/res_owner/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class RestaurantOwner(models.Model):
+    """A model representation of a restaurant owner"""
+    # There is no way a person's name is that long
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=254)
+    password = models.CharField(max_length=128)
+
+    # For user...
+    associating_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return the name of the owner"""
+        return self.name
+
+
+class Restaurant(models.Model):
+    """A model representation of a restaurant"""
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)  # May subject to change
+
+
