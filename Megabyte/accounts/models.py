75d91d01c410a40812675f34149c856be3acd39f
Huy Nguyen <dshk127mm@gmail.com>
2023-10-20 22:21:33 -0400

    Created the stage for user registration

--- Megabyte/accounts/models.py
+++ Megabyte/accounts/models.py
@@ -1,3 +1,4 @@
 from django.db import models
-
+from ..res_owner.models import Restaurant
+from ..user.models import UserData, Quantity, ShoppingCart, Location, Order
 # Create your models here.
---
+++ Megabyte/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from django.contrib.auth import urls
+from . import views
+
+app_name = 'accounts'
+urlpatterns = []
