3f1a2fca87ba3a99bbffc9665c9d23a53e226156
Huy Nguyen <dshk127mm@gmail.com>
2023-11-01 21:49:11 -0400

    Now password change can actually work now.

--- Megabyte/accounts/views.py
+++ Megabyte/accounts/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeView
 from django.http import Http404
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
 from .models import CustomUser
+from django.urls import reverse_lazy
 from res_owner.models import Restaurant
 # Do same thing for User later.
 
 # Create your views here.
+
+
+class PasswordsChangeView(PasswordChangeView):
+    """Custom password change view"""
+    form_class = PasswordChangeForm
+    # Go to the home_page URLs in the urls.py of this app once password changing is done
+    success_url = reverse_lazy(viewname='accounts:home_page')
 
 
 def register(request):
@@ -66,7 +76,7 @@ def home_page(request):
     Edit the user's info
     :param request: a HttpRequest object specific to Django
     """
-    # No user
+    # No user => Redirect to the welcome one
     if request.user.is_anonymous:
         return redirect('accounts:ellis')
     if request.user.is_res_owner is True:
