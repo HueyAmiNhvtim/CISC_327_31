a0733ddeed389eb7b9434f1cdca2f0775f6d43d6
Gulchiszz <114819866+Gulchiszz@users.noreply.github.com>
2023-11-03 09:04:36 +0800

    add account tests

---
+++ Megabyte/accounts/tests/test_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+
+class CustomUserModelTest(TestCase):
+    def test_create_user(self):
+        user = CustomUser.objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='testpassword'
+        )
+        self.assertEqual(user.username, 'testuser')
+        self.assertFalse(user.is_staff)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_res_owner)
+
+    def test_create_superuser(self):
+        admin = CustomUser.objects.create_superuser(
+            email='admin@example.com',
+            username='adminuser',
+            password='adminpassword'
+        )
+        self.assertEqual(admin.username, 'adminuser')
+        self.assertTrue(admin.is_staff)
+        self.assertTrue(admin.is_active)
+        self.assertTrue(admin.is_superuser)
+        self.assertTrue(admin.is_res_owner)
---
+++ Megabyte/accounts/tests/test_views.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AccountViewsTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='testpassword',
+            is_res_owner=False
+        )
+
+    def test_register_view(self):
+        response = self.client.get(reverse('accounts:register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Register')
+
+    def test_user_home_page_view(self):
+        self.client.login(email='test@example.com', password='testpassword')
+        response = self.client.get(reverse('accounts:home_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_res_owner_home_page_view(self):
+        self.client.login(email='test@example.com', password='testpassword')
+        response = self.client.get(reverse('accounts:home_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_user_view(self):
+        self.client.login(email='test@example.com', password='testpassword')
+        response = self.client.get(reverse('accounts:edit_user'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_change_view(self):
+        self.client.login(email='test@example.com', password='testpassword')
+        response = self.client.post(reverse('accounts:change_password', kwargs={'user_id': self.user.id}), data={'user_id': self.user.id})
+        self.assertEqual(response.status_code, 200)
