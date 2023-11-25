c1ebeb706625e1d292b4445c3a53f154e355d864
Gulchiszz <114819866+Gulchiszz@users.noreply.github.com>
2023-11-25 10:25:09 +0800

    update white box test

--- Megabyte/accounts/tests/test_01_regist_sel.py
+++ Megabyte/accounts/tests/test_01_regist_sel.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 class RegistTest(LiveServerTestCase):
 
     def setUp(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--enable-features=NetworkService")
         self.driver = webdriver.Chrome()
         self.driver.get('http://127.0.0.1:8000/')
         self.driver.maximize_window()  #
@@ -24,8 +26,7 @@ class RegistTest(LiveServerTestCase):
         browser.find_element(By.NAME, 'password2').send_keys('testpassword')        
         browser.find_element(By.NAME, 'submit').click()
         # check result
-        result = browser.find_elements(By.NAME, 'user_settings_button')
-        self.assertTrue(len(result), 1)
+        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/user/') 
 
 
     def test_regist_02_res_owner(self):
@@ -42,7 +43,17 @@ class RegistTest(LiveServerTestCase):
         result = browser.find_elements(By.NAME, 'res_settings_button')
         self.assertTrue(len(result), 1)
 
-
+    def test_regist_03_failure(self):
+        # failure case
+        browser = self.driver
+        browser.find_element(By.LINK_TEXT, 'Register').click()
+        browser.find_element(By.NAME, 'email').send_keys('test111@123.com')
+        browser.find_element(By.NAME, 'username').send_keys('test111')
+        browser.find_element(By.NAME, 'password1').send_keys('testpassword')
+        browser.find_element(By.NAME, 'password2').send_keys('testpassword')        
+        browser.find_element(By.NAME, 'submit').click()
+        # check result        
+        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/register/')  # failure regist, the current url still is 'http://127.0.0.1:8000/register/'
         
 
 
--- Megabyte/accounts/tests/test_02_login_sel.py
+++ Megabyte/accounts/tests/test_02_login_sel.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 class LoginTest(LiveServerTestCase):
 
     def setUp(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--enable-features=NetworkService")
         self.driver = webdriver.Chrome()
         self.driver.get('http://127.0.0.1:8000/')
         self.driver.maximize_window()  #
@@ -22,8 +24,18 @@ class LoginTest(LiveServerTestCase):
         browser.find_element(By.NAME, 'password').send_keys('testpassword')
         browser.find_element(By.NAME, 'submit').click()
         # check result
-        result = browser.find_elements(By.NAME, 'user_settings_button')
-        self.assertTrue(len(result), 1)
+        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/home/')
+    
+    def test_login_failure(self):
+        # failure case
+        browser = self.driver
+        # login
+        browser.find_element(By.LINK_TEXT, 'Login').click()
+        browser.find_element(By.NAME, 'username').send_keys('test111@123.com')
+        browser.find_element(By.NAME, 'password').send_keys('test')
+        browser.find_element(By.NAME, 'submit').click()
+        # check result
+        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/user_stuff/login/') # failure login, current url still is 'http://127.0.0.1:8000/user_stuff/login/'
 
 
 
