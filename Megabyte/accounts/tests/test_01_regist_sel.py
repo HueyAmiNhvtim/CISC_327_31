f6c297c4ec19856b16d12e41e33e85b769975baf
Gulchiszz <114819866+Gulchiszz@users.noreply.github.com>
2023-11-26 12:50:44 +0800

    fix test code

--- Megabyte/accounts/tests/test_01_regist_sel.py
+++ Megabyte/accounts/tests/test_01_regist_sel.py
@@ -9,7 +9,7 @@ class RegistTest(LiveServerTestCase):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--enable-features=NetworkService")
         self.driver = webdriver.Chrome()
-        self.driver.get('http://127.0.0.1:8000/')
+        self.driver.get(self.live_server_url)
         self.driver.maximize_window()  #
     
     def tearDown(self):
@@ -26,7 +26,7 @@ class RegistTest(LiveServerTestCase):
         browser.find_element(By.NAME, 'password2').send_keys('testpassword')        
         browser.find_element(By.NAME, 'submit').click()
         # check result
-        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/user/') 
+        self.assertEqual(browser.current_url, f'{self.live_server_url}/user/') 
 
 
     def test_regist_02_res_owner(self):
@@ -49,11 +49,11 @@ class RegistTest(LiveServerTestCase):
         browser.find_element(By.LINK_TEXT, 'Register').click()
         browser.find_element(By.NAME, 'email').send_keys('test111@123.com')
         browser.find_element(By.NAME, 'username').send_keys('test111')
-        browser.find_element(By.NAME, 'password1').send_keys('testpassword')
-        browser.find_element(By.NAME, 'password2').send_keys('testpassword')        
+        browser.find_element(By.NAME, 'password1').send_keys('testpassword1')
+        browser.find_element(By.NAME, 'password2').send_keys('testpassword2')        
         browser.find_element(By.NAME, 'submit').click()
         # check result        
-        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/register/')  # failure regist, the current url still is 'http://127.0.0.1:8000/register/'
+        self.assertEqual(browser.current_url, f'{self.live_server_url}/register/')  # failure regist, the current url still is 'http://127.0.0.1:8000/register/'
         
 
 
--- Megabyte/accounts/tests/test_02_login_sel.py
+++ Megabyte/accounts/tests/test_02_login_sel.py
@@ -1,4 +1,5 @@
 import time
+from django.contrib.auth import get_user_model
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,10 +7,11 @@ from selenium.webdriver.common.by import By
 class LoginTest(LiveServerTestCase):
 
     def setUp(self):
+        self.CustomUserModel = get_user_model()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--enable-features=NetworkService")
         self.driver = webdriver.Chrome()
-        self.driver.get('http://127.0.0.1:8000/')
+        self.driver.get(self.live_server_url)
         self.driver.maximize_window()  #
     
     def tearDown(self):
@@ -18,13 +20,16 @@ class LoginTest(LiveServerTestCase):
 
     def test_login(self):
         browser = self.driver
+        self.CustomUserModel.objects.create_user(email='test111@123.com',
+                                                 username='test111', is_res_owner=False,
+                                                 password='testpassword')
         # login
         browser.find_element(By.LINK_TEXT, 'Login').click()
         browser.find_element(By.NAME, 'username').send_keys('test111@123.com')
         browser.find_element(By.NAME, 'password').send_keys('testpassword')
         browser.find_element(By.NAME, 'submit').click()
         # check result
-        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/home/')
+        self.assertEqual(browser.current_url, f'{self.live_server_url}/home/')
     
     def test_login_failure(self):
         # failure case
@@ -35,7 +40,7 @@ class LoginTest(LiveServerTestCase):
         browser.find_element(By.NAME, 'password').send_keys('test')
         browser.find_element(By.NAME, 'submit').click()
         # check result
-        self.assertEqual(browser.current_url, 'http://127.0.0.1:8000/user_stuff/login/') # failure login, current url still is 'http://127.0.0.1:8000/user_stuff/login/'
+        self.assertEqual(browser.current_url, f'{self.live_server_url}/user_stuff/login/') # failure login, current url still is 'http://127.0.0.1:8000/user_stuff/login/'
 
 
 
